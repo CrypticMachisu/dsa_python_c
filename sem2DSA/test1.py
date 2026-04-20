44dd6e920ca5d583145d68009030d63db4cb44ff
CrypticMachisu <machisuroy09@gmail.com>
2026-04-20 12:09:40 +0300

    Basic code

--- sem2DSA/test1.py
+++ sem2DSA/test1.py
@@ -1,1 +1,42 @@
-print("Hello World")
+# print("Hello World")
+
+# name = "Jojo"
+# name2 = "Jojo "
+
+# #You do not need to declare a data type in Python
+# #The data type is automatically assigned based on the value assigned to the variable
+# age = 20
+# balance = 159.258
+# height = 5.9
+# flag = True
+
+# print(id(name))
+# print(id(name2))
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(balance))
+# print(type(flag))
+
+
+
+# # Java Code
+# # for (int i = 0; i < 10; i++){
+# #     Sout.print(i)
+# # }
+
+# for i in range(5):
+#     print(i)
+print("Using range")
+for i in range(8):
+    print(i)
+print("\n")
+for i in range(18):
+    if i == 17:
+        print(i)
+
+# print("Using items")
+# items = [1,2,3,4,5,6,7,8]
+# for item in items:
+#     print(item)
+
